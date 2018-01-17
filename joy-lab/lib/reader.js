20d99a4d3d94f45418351fda74fcb428fb9c4ff9
strawbee <yuejiao.hou@gmail.com>
2018-01-17 15:52:16 -0800

    being confused

--- joy-lab/lib/reader.js
+++ joy-lab/lib/reader.js
@@ -1,3 +1,21 @@
 'use strict';
+const fs = require('fs');
 
-exports.
+exports.read = (paths, callback, res) => {
+    if (!res) res = [];
+    if (!paths[0]) return callback(null, res);
+    else {
+        fs.readFile(paths.shift(), (err, data) => {
+            if (err) console.error(err);
+            res.push(data.toString());
+            return exports.read(paths, callback, res);
+        });
+    }
+}
+
+let someVar = exports.read([`${__dirname}/../assets/tacos.html`, `${__dirname}/../assets/burritos.html`, `${__dirname}/../assets/quesadillas.html`], (err, data) => {
+    if (err) console.error(err);
+    console.log(data);
+    return data;
+});
+console.log(someVar); // why does it return undefined?
