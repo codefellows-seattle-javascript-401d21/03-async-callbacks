4df45df5a7147d6690c558ad4da4055878e4e938
Ender Smith <Ender Smith>
2018-01-17 22:42:03 -0800

    lab-ender 03

---
+++ lab-ender/lib/reader.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const fs = require(`fs`);
+const validate = require(`./validate`);
+
+const fileContents = [];
+let error;
+
+const reader = (pathsArr, cb) => {
+  if (!validate(pathsArr)) return null;
+  const filePath = pathsArr.shift();
+  fs.readFile(`${__dirname}/${filePath}`, `utf8`, (err, data) => {
+    fileContents.push(data.toString());
+    if (pathsArr.length !== 0) reader(pathsArr, cb);
+    else cb(null, fileContents);
+  });
+};
+
+reader([
+  `../assets/first.txt`,
+  `../assets/third.txt`,
+  `../assets/second.txt`
+], (err, fd) => {
+  if (err) return err;
+  return fd;
+});
+
+module.exports = reader;
