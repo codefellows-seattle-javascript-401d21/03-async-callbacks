2d6a651bf57638acbf27c0f6284549d960a6e49f
Koko Kassa <kowserk7@gmail.com>
2018-01-17 21:45:07 -0800

    changed filetypes

--- lab-koko/__test__/reader.test.js
+++ lab-koko/__test__/reader.test.js
@@ -1,23 +1,29 @@
 'use strict';
 
 const reader = require('../lib/reader.js');
-require('jest');
 
-describe('File System Lib', function() {
+
+let arrOne = [`${__dirname}../assets/one.txt`, `${__dirname}../assets/two.txt`,`${__dirname}../assets/three.txt`];
+let arrTwo = ['problem one', 'problem two', 'problem three'];
+
+describe('File System Module', function() {
+  describe('#reader', function () {
+
+  })
   it('should do a thing asyncronously', (done) => {
     // When testing async code:
 
-    // Using the done keyword
-    reader.doAThingAsyn(data, (err, fd) => {
-      if(err) console.log(err);
-      expect(fd).toBe(true);
-      done();
-    });
+//     // Using the done keyword
+//     reader.doAThingAsyn(data, (err, fd) => {
+//       if(err) console.log(err);
+//       expect(fd).toBe(true);
+//       done();
+//     });
 
-    // Without the done keyword (don't need the done param in the IT block)
-    return reader.doAThingAsyn(data, (err, fd) => {
-      if(err) console.log(err);
-      expect(fd).toBe(true);
-    });
-  });
-});
+//     // Without the done keyword (don't need the done param in the IT block)
+//     return reader.doAThingAsyn(data, (err, fd) => {
+//       if(err) console.log(err);
+//       expect(fd).toBe(true);
+//     });
+//   });
+// });
