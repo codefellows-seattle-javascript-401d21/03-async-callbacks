7a717040f41cdfbdcd80e30b4f2095e034ca4bac
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-01-17 19:02:40 -0800

    add first function

--- lab-ed/lib/reader.js
+++ lab-ed/lib/reader.js
@@ -1,5 +1,14 @@
-module.exports.reader = reader
+'use strict'
 
-function reader() {
+const fs = require('fs')
 
-}
+let paths = [`${__dirname}/data/one.html`, `${__dirname}/data/two.html`, `${__dirname}/data/three.html`]
+
+let mappedFiles = []
+
+fs.readFile(paths[0], (err, data) => {
+  if(err) console.error(err)
+  let fd = data.toString()
+  mappedFiles.push(fd)
+  console.log(mappedFiles)
+})
