a34a5a8fe774ac6f2799c376ebc00c6ad1fff25f
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-01-17 22:12:00 -0800

    made test runs and functioning properly

--- lab-yohanes/__test__/reader.test.js
+++ lab-yohanes/__test__/reader.test.js
@@ -6,3 +6,37 @@ require('jest');
 let arrayOne = [`${__dirname}/../assets/doing.txt`, `${__dirname}/../assets/reading.txt`, `${__dirname}/../assets/something.txt`];
 let arrayTwo = ['prollem one', 'prollem two', 'prollem three'];
 
+describe('reader module', function () {
+  describe('#reader', function () {
+    it('should return an array (object)', function () {
+      return reader(arrayOne, (err, fd) => {
+        if (err) console.error(err);
+        expect(typeof fd).toBe('object');
+      });
+    });
+    it('it should return strings', function () {
+      return reader(arrayOne, (err, fd) => {
+        if (err) console.error(err);
+        expect(typeof fd[0]).toBe('string');
+      });
+    });
+    it('should return the strings in the same order', function () {
+      return reader(arrayOne, (err, fd) => {
+        if (err) console.error(err);
+        expect(fd[0].includes('#1')).toBe(true);
+      });
+      return reader(arrayOne, (err, fd) => {
+        if (err) console.error(err);
+        expect(fd[1].includes('#2')).toBe(true);
+      });
+      return reader(arrayOne, (err, fd) => {
+        if (err) console.error(err);
+        expect(fd[2].includes('#3')).toBe(true);
+      });
+    });
+    it('should throw an error if the input is not valid path', function () {
+     
+    });
+  });
+});
+
