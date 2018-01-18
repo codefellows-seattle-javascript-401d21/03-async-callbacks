9ee97f4bf874ccb3b3d030d01af98ca25186c8df
Steven Carpenter <stevegcarpenter@gmail.com>
2018-01-17 23:44:43 -0800

    Added tests for the readFiles method

---
+++ lab-steve/__test__/reader.test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const reader = require('../lib/reader');
+
+describe('Reader Module', () => {
+  describe('#readFiles', () => {
+    it('should read and store a variable number of files in order from an array', (done) => {
+      reader.readFiles(['../assets/a.txt', '../assets/b.txt', '../assets/c.txt', '../assets/d.txt'], (err, data) => {
+        if (err) console.log(err);
+        expect(data[0]).toBe('a\n');
+        expect(data[1]).toBe('b\n');
+        expect(data[2]).toBe('c\n');
+        expect(data[3]).toBe('d\n');
+        done();
+      });
+    });
+
+    it('should return undefined data when one or more filepaths is invalid', (done) => {
+      reader.readFiles(['../assets/foo.txt'], (err, data) => {
+        if (err) console.log(err);
+        expect(data).toBe(undefined);
+        done();
+      });
+
+      reader.readFiles(['../assets/a.txt', '../assets/foo.txt'], (err, data) => {
+        if (err) console.log(err);
+        expect(data).toBe(undefined);
+        done();
+      });
+    });
+  });
+});
