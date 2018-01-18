d7f29047bdc680b873a410d421fd79e2aa122b9f
Melanie Downing <sayanything830@gmail.com>
2018-01-17 17:10:06 -0800

    added content to files, working on reader code

--- lab-melanie/lib/reader.js
+++ lab-melanie/lib/reader.js
@@ -1,3 +1,21 @@
 'use strict';
 
 const reader = module.exports = {};
+const fs = require('fs');
+
+let lorem = [`${__dirname}/../assests/red.html`, `${__dirname}/../assests/green.html`, `${__dirname}/../assests/blue.html`];
+
+reader.newLorem = function(arr) {
+  for(let i in arr) {
+    fs.readFile(`${arr[i]}`, (error, data) => {
+      if(err) console.error(err);
+      let fd = data.toString();
+      console.log(fd);
+
+      fs.writeFile(`${__dirname}/../assets/new.html`, err => err ? console.error(err) : undefined);
+
+    });
+  }
+};
+
+reader.newLorem(lorem);
