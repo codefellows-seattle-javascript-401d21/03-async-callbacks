735b2a5c68d08393070cc9f840bfa0baeb422512
billodell <billodell5@gmail.com>
2018-01-17 16:58:14 -0800

    array populating correctly

--- lab-bill/lib/reader.js
+++ lab-bill/lib/reader.js
@@ -1,8 +1,44 @@
 const fs = require('fs');
 
-module.exports = function reader(arr) {
-    fs.readFile(`${__dirname}/data/one.html`, (err, data) => {
-        if(err) console.error(err)
-};
+// module.exports = function reader() {
+//   fs.readFile(`${__dirname}/assets/first.html`, (err, data) => {
+//     if(err) console.error(err);
+//     console.log(data);
+    
+//   });
+// };
+// reader();
 
-let testArray = [`${__dirname}/`]
+function reader(paths) {
+  let result = [];
+  paths.forEach(function(filepath, i) {
+    fs.readFile(filepath, (err, data) => {
+      if(err) console.error(err);
+      // console.log(data.toString('utf-8'));
+      console.log('file' + i);
+     
+      result[i] = (data.toString('utf-8'));
+      console.log(result[1])
+      // console.log(result);
+      // callback(null, result);
+    });
+  });
+}
+let paths = [`${__dirname}/../assets/first.html`, `${__dirname}/../assets/second.html`, `${__dirname}/../assets/third.html`];
+reader(paths);
+// function reader(paths, callback) {
+//   let len = arr.length
+//   if(!len) {
+//     fs.readFile(i, (err, data) => {
+//       if(err) console.error(err);
+//       console.log(data.toString('utf-8')); 
+//     });
+//   });
+// }
+
+// function reader(){
+//   fs.readFile(`${__dirname}/../assets/first.html`, (err, data) => {
+//     if(err) console.error(err);
+//     console.log(data);
+//   });
+// }
