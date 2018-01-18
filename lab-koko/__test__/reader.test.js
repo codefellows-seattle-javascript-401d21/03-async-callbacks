e39572b6d040dce866de6c60a08c2d35a0cc6f0d
Koko Kassa <kowserk7@gmail.com>
2018-01-17 22:08:36 -0800

    added test cases

--- lab-koko/__test__/reader.test.js
+++ lab-koko/__test__/reader.test.js
@@ -3,27 +3,34 @@
 const reader = require('../lib/reader.js');
 
 
-let arrOne = [`${__dirname}../assets/one.txt`, `${__dirname}../assets/two.txt`,`${__dirname}../assets/three.txt`];
+let arrOne = [`${__dirname}/../assets/one.txt`, `${__dirname}/../assets/two.txt`,`${__dirname}/../assets/three.txt`];
 let arrTwo = ['problem one', 'problem two', 'problem three'];
 
 describe('File System Module', function() {
-  describe('#reader', function () {
-
-  })
-  it('should do a thing asyncronously', (done) => {
-    // When testing async code:
-
-//     // Using the done keyword
-//     reader.doAThingAsyn(data, (err, fd) => {
-//       if(err) console.log(err);
-//       expect(fd).toBe(true);
-//       done();
-//     });
-
-//     // Without the done keyword (don't need the done param in the IT block)
-//     return reader.doAThingAsyn(data, (err, fd) => {
-//       if(err) console.log(err);
-//       expect(fd).toBe(true);
-//     });
-//   });
-// });
+  describe('#reader', function () { 
+    it('should return an array (object)', function ()  {
+      return reader(arrOne, (err, fd) => {
+        if(err) console.log(err);
+        expect(typeof fd).toBe('object');
+      });
+    });
+    it('should return an strings' , function ()  {
+      return reader(arrOne, (err, fd) => {
+        if(err) console.log(err);
+        expect(typeof fd).toBe('string');
+      });
+    });
+    it ('should return the strings in the same order', function () {
+      return reader(arrOne, (err, fd) => {
+        if(err) console.log(err);
+        expect(typeof fd).toBe(true);
+      });
+    });
+    it ('should throw an error in thr input is not a valid path', function () {
+      return reader (arrTwo, (err, fd) => {
+        if (err) console.error(err);
+        expect(reader()).toThrowError();
+      });
+    });
+  });
+});
