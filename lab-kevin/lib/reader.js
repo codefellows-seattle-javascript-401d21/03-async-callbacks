3c6f0fccf338d0a52e71114e74fc49e2f67ec05b
Kevin Miller <kevin@driftabout.com>
2018-01-17 19:24:26 -0800

    reader working

--- lab-kevin/lib/reader.js
+++ lab-kevin/lib/reader.js
@@ -1,7 +1,20 @@
 const fs = require('fs');
 
 module.exports = (path_array) => {
+  let results = [];
 
-  console.log('reader paths to read', path_array);  
+  readFile(path_array, readFile);
+  
+  function readFile(path_array, callback){
+    if(!path_array.length) return console.log(results);
+    fs.readFile(path_array.shift(), (err, data) => {
+      if (err) return console.error(err);
+      results.push(data.toString());
+      callback(path_array, readFile);
+    });
+  }
 
 };
+
+
+
