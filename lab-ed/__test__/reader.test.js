a99d7d44cce5c308faa05f2c1d8b0c3a40b7612e
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-01-17 20:22:53 -0800

    add first test

--- lab-ed/__test__/reader.test.js
+++ lab-ed/__test__/reader.test.js
@@ -1,3 +1,12 @@
 'use strict'
 
-const reader = require('./lib/reader')
+const reader = require('../lib/reader')
+
+describe('Reader Module', function() {
+  describe('#Mapfiles', function() {
+    
+    it('should return null if not being passed an array', function() {
+      expect(reader.mapFiles()).toBe(null)
+    })
+  })
+})
--- lab-ed/lib/reader.js
+++ lab-ed/lib/reader.js
@@ -2,13 +2,33 @@
 
 const fs = require('fs')
 
-let paths = [`${__dirname}/data/one.html`, `${__dirname}/data/two.html`, `${__dirname}/data/three.html`]
-
+// let paths = [`${__dirname}/data/one.html`, `${__dirname}/data/two.html`, `${__dirname}/data/three.html`]
 let mappedFiles = []
+let paths = []
 
-fs.readFile(paths[0], (err, data) => {
-  if(err) console.error(err)
-  let fd = data.toString()
-  mappedFiles.push(fd)
-  console.log(mappedFiles)
-})
+exports.mapFiles = function(error, callback) {
+  if (paths.length === 0) return null
+  let i = 0
+  fs.readFile(paths[i], (error, data) => {
+    if(error) console.error(error)
+    let fd = data.toString()
+    mappedFiles.push(fd)
+    i++
+
+    fs.readFile(paths[i], (error, data) => {
+      if(error) console.error(error)
+      let fd = data.toString()
+      mappedFiles.push(fd)
+      i++
+
+      fs.readFile(paths[i], (error, data) => {
+        if(error) console.error(error)
+        let fd = data.toString()
+        mappedFiles.push(fd)
+        console.log(mappedFiles)
+        callback(error, mappedFiles)
+      })
+    })
+  })
+}
+
