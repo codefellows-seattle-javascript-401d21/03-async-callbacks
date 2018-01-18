8a78ff20414acdeb9b2954b70e5605c961948062
Steven Carpenter <stevegcarpenter@gmail.com>
2018-01-17 23:45:19 -0800

    Recursive async function to read list of files

--- lab-steve/lib/reader.js
+++ lab-steve/lib/reader.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const fs = require('fs');
+const reader = module.exports = {};
+
+reader.readFiles = readFiles;
+var result = [];
+  
+function readFiles(paths, callback) {
+  // get first path
+  let path = paths.splice(0, 1)[0];
+  // all paths processed
+  if (!path) {
+    callback(null, result);
+    return;
+  }
+    
+  fs.readFile(`${__dirname}/${path}`, (err, data) => {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    result.push(data.toString());
+    readFiles(paths, callback);
+  });
+}
